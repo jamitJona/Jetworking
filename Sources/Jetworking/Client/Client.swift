071c6d876be8bfb7d8d5b62e792b2b52c508d982
Jens Krug <jens.krug@jamitlabs.com>
2020-10-27 22:13:58 +0100

    Refactor request execution to reduce lines of code

--- Sources/Jetworking/Client/Client.swift
+++ Sources/Jetworking/Client/Client.swift
@@ -90,18 +90,9 @@ public final class Client {
 
     // MARK: - Methods
     @discardableResult
-    public func get<ResponseType>(endpoint: Endpoint<ResponseType>, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
+    public func get<ResponseType: Decodable>(endpoint: Endpoint<ResponseType>, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
         do {
-            let request: URLRequest = try createRequest(forHttpMethod: .GET, and: endpoint)
-            return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
-                self?.handleResponse(
-                    data: data,
-                    urlResponse: urlResponse,
-                    error: error,
-                    endpoint: endpoint,
-                    completion: completion
-                )
-            }
+            return try makeAndExecuteRequest(with: .GET, and: endpoint, completion: completion)
         } catch {
             enqueue(completion(nil, .failure(error)))
         }
@@ -110,19 +101,9 @@ public final class Client {
     }
 
     @discardableResult
-    public func post<BodyType: Encodable, ResponseType>(endpoint: Endpoint<ResponseType>, body: BodyType, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
+    public func post<BodyType: Encodable, ResponseType: Decodable>(endpoint: Endpoint<ResponseType>, body: BodyType, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
         do {
-            let bodyData: Data = try configuration.encoder.encode(body)
-            let request: URLRequest = try createRequest(forHttpMethod: .POST, and: endpoint, and: bodyData)
-            return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
-                self?.handleResponse(
-                    data: data,
-                    urlResponse: urlResponse,
-                    error: error,
-                    endpoint: endpoint,
-                    completion: completion
-                )
-            }
+            return try makeAndExecuteRequest(with: .POST, and: endpoint, and: body, completion: completion)
         } catch {
             enqueue(completion(nil, .failure(error)))
         }
@@ -131,19 +112,9 @@ public final class Client {
     }
 
     @discardableResult
-    public func put<BodyType: Encodable, ResponseType>(endpoint: Endpoint<ResponseType>, body: BodyType, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
+    public func put<BodyType: Encodable, ResponseType: Decodable>(endpoint: Endpoint<ResponseType>, body: BodyType, _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
         do {
-            let bodyData: Data = try configuration.encoder.encode(body)
-            let request: URLRequest = try createRequest(forHttpMethod: .PUT, and: endpoint, and: bodyData)
-            return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
-                self?.handleResponse(
-                    data: data,
-                    urlResponse: urlResponse,
-                    error: error,
-                    endpoint: endpoint,
-                    completion: completion
-                )
-            }
+            return try makeAndExecuteRequest(with: .PUT, and: endpoint, and: body, completion: completion)
         } catch {
             enqueue(completion(nil, .failure(error)))
         }
@@ -152,23 +123,13 @@ public final class Client {
     }
 
     @discardableResult
-    public func patch<BodyType: Encodable, ResponseType>(
+    public func patch<BodyType: Encodable, ResponseType: Decodable>(
         endpoint: Endpoint<ResponseType>,
         body: BodyType,
         _ completion: @escaping RequestCompletion<ResponseType>
     ) -> CancellableRequest? {
         do {
-            let bodyData: Data = try configuration.encoder.encode(body)
-            let request: URLRequest = try createRequest(forHttpMethod: .PATCH, and: endpoint, and: bodyData)
-            return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
-                self?.handleResponse(
-                    data: data,
-                    urlResponse: urlResponse,
-                    error: error,
-                    endpoint: endpoint,
-                    completion: completion
-                )
-            }
+            return try makeAndExecuteRequest(with: .PATCH, and: endpoint, and: body, completion: completion)
         } catch {
             enqueue(completion(nil, .failure(error)))
         }
@@ -177,18 +138,9 @@ public final class Client {
     }
 
     @discardableResult
-    public func delete<ResponseType>(endpoint: Endpoint<ResponseType>, parameter: [String: Any] = [:], _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
+    public func delete<ResponseType: Decodable>(endpoint: Endpoint<ResponseType>, parameter: [String: Any] = [:], _ completion: @escaping RequestCompletion<ResponseType>) -> CancellableRequest? {
         do {
-            let request: URLRequest = try createRequest(forHttpMethod: .DELETE, and: endpoint)
-            return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
-                self?.handleResponse(
-                    data: data,
-                    urlResponse: urlResponse,
-                    error: error,
-                    endpoint: endpoint,
-                    completion: completion
-                )
-            }
+            return try makeAndExecuteRequest(with: .DELETE, and: endpoint, completion: completion)
         } catch {
             enqueue(completion(nil, .failure(error)))
         }
@@ -196,6 +148,108 @@ public final class Client {
         return nil
     }
 
+    func makeAndExecuteRequest<ResponseType: Decodable>(with method: HTTPMethod,
+        and endpoint: Endpoint<ResponseType>,
+        completion: @escaping RequestCompletion<ResponseType>
+    ) throws -> CancellableRequest? {
+        let request: URLRequest = try createRequest(forHttpMethod: method, and: endpoint)
+        return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
+            self?.handleResponse(
+                data: data,
+                urlResponse: urlResponse,
+                error: error,
+                completion: completion
+            )
+        }
+    }
+
+    func makeAndExecuteRequest<ResponseType: Decodable, BodyType: Encodable>(
+        with method: HTTPMethod,
+        and endpoint: Endpoint<ResponseType>,
+        and body: BodyType,
+        completion: @escaping RequestCompletion<ResponseType>
+    ) throws -> CancellableRequest? {
+        let bodyData: Data = try configuration.encoder.encode(body)
+        let request: URLRequest = try createRequest(forHttpMethod: method, and: endpoint, and: bodyData)
+        return requestExecutor.send(request: request) { [weak self] data, urlResponse, error in
+            self?.handleResponse(
+                data: data,
+                urlResponse: urlResponse,
+                error: error,
+                completion: completion
+            )
+        }
+    }
+    
+    private func checkForValidDownloadURL(_ url: URL) -> Bool {
+        guard let scheme = URLComponents(string: url.absoluteString)?.scheme else { return false }
+
+        return scheme == "http" || scheme == "https"
+    }
+
+    private func createRequest<ResponseType>(
+        forHttpMethod httpMethod: HTTPMethod,
+        and endpoint: Endpoint<ResponseType>,
+        and body: Data? = nil
+    ) throws -> URLRequest {
+        let request = URLRequest(
+            url: try URLFactory.makeURL(from: endpoint, withBaseURL: configuration.baseURL),
+            httpMethod: httpMethod,
+            httpBody: body
+        )
+        return configuration.requestInterceptors.reduce(request) { request, interceptor in
+            return interceptor.intercept(request)
+        }
+    }
+
+    // TODO: Improve this function (Error handling, evaluation of header fields, status code evalutation, ...)
+    private func handleResponse<ResponseType: Decodable>(
+        data: Data?,
+        urlResponse: URLResponse?,
+        error: Error?,
+        completion: @escaping RequestCompletion<ResponseType>
+    ) {
+        let interceptedResponse = configuration.responseInterceptors.reduce(urlResponse) { response, component in
+            return component.intercept(data: data, response: response, error: error)
+        }
+
+        guard let currentURLResponse = interceptedResponse as? HTTPURLResponse else {
+            return enqueue(completion(nil, .failure(error ?? APIError.responseMissing)))
+        }
+
+        if let error = error { return enqueue(completion(currentURLResponse, .failure(error))) }
+
+        switch HTTPStatusCodeType(statusCode: currentURLResponse.statusCode) {
+        case .successful:
+            guard
+                let data = data,
+                let decodedData = try? configuration.decoder.decode(ResponseType.self, from: data)
+            else {
+                return enqueue(completion(currentURLResponse, .failure(APIError.decodingError)))
+            }
+            // Evaluate Header fields --> urlResponse.allHeaderFields
+
+            enqueue(completion(currentURLResponse, .success(decodedData)))
+            
+        case .clientError, .serverError:
+            guard let error = error else { return completion(currentURLResponse, .failure(APIError.unexpectedError)) }
+
+            enqueue(completion(currentURLResponse, .failure(error)))
+
+        default:
+            return
+        }
+    }
+
+    private func enqueue(_ completion: @escaping @autoclosure () -> Void) {
+        configuration.responseQueue.async {
+            completion()
+        }
+    }
+}
+
+// MARK: - Download handling
+extension Client {
     @discardableResult
     public func download(
         url: URL,
@@ -216,6 +270,33 @@ public final class Client {
         return task
     }
 
+}
+extension Client: DownloadExecutorDelegate {
+    public func downloadExecutor(
+        _ downloadTask: URLSessionDownloadTask,
+        didWriteData bytesWritten: Int64,
+        totalBytesWritten: Int64,
+        totalBytesExpectedToWrite: Int64
+    ) {
+        guard let progressHandler = executingDownloads[downloadTask.identifier]?.progressHandler else { return }
+        enqueue(progressHandler(totalBytesWritten, totalBytesExpectedToWrite))
+    }
+
+    public func downloadExecutor(_ downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        // TODO handle response before calling the completion
+        guard let completionHandler = executingDownloads[downloadTask.identifier]?.completionHandler else { return }
+        enqueue(completionHandler(location, downloadTask.response, downloadTask.error))
+    }
+
+    public func downloadExecutor(_ downloadTask: URLSessionDownloadTask, didCompleteWithError error: Error?) {
+        // TODO handle response before calling the completion
+        guard let completionHandler = executingDownloads[downloadTask.identifier]?.completionHandler else { return }
+        enqueue(completionHandler(nil, downloadTask.response, error))
+    }
+}
+
+// MARK: - Upload handling
+extension Client {
     @discardableResult
     public func upload(
         url: URL,
@@ -267,97 +348,6 @@ public final class Client {
             )
         }
         return task
-    }
-    
-    private func checkForValidDownloadURL(_ url: URL) -> Bool {
-        guard let scheme = URLComponents(string: url.absoluteString)?.scheme else { return false }
-
-        return scheme == "http" || scheme == "https"
-    }
-
-    private func createRequest<ResponseType>(
-        forHttpMethod httpMethod: HTTPMethod,
-        and endpoint: Endpoint<ResponseType>,
-        and body: Data? = nil
-    ) throws -> URLRequest {
-        let request = URLRequest(
-            url: try URLFactory.makeURL(from: endpoint, withBaseURL: configuration.baseURL),
-            httpMethod: httpMethod,
-            httpBody: body
-        )
-        return configuration.requestInterceptors.reduce(request) { request, interceptor in
-            return interceptor.intercept(request)
-        }
-    }
-
-    // TODO: Improve this function (Error handling, evaluation of header fields, status code evalutation, ...)
-    private func handleResponse<ResponseType>(
-        data: Data?,
-        urlResponse: URLResponse?,
-        error: Error?,
-        endpoint: Endpoint<ResponseType>,
-        completion: @escaping RequestCompletion<ResponseType>
-    ) {
-        let interceptedResponse = configuration.responseInterceptors.reduce(urlResponse) { response, component in
-            return component.intercept(data: data, response: response, error: error)
-        }
-
-        guard let currentURLResponse = interceptedResponse as? HTTPURLResponse else {
-            return enqueue(completion(nil, .failure(error ?? APIError.responseMissing)))
-        }
-
-        if let error = error { return enqueue(completion(currentURLResponse, .failure(error))) }
-
-        switch HTTPStatusCodeType(statusCode: currentURLResponse.statusCode) {
-        case .successful:
-            guard
-                let data = data,
-                let decodedData = try? configuration.decoder.decode(ResponseType.self, from: data)
-            else {
-                return enqueue(completion(currentURLResponse, .failure(APIError.decodingError)))
-            }
-            // Evaluate Header fields --> urlResponse.allHeaderFields
-
-            enqueue(completion(currentURLResponse, .success(decodedData)))
-            
-        case .clientError, .serverError:
-            guard let error = error else { return completion(currentURLResponse, .failure(APIError.unexpectedError)) }
-
-            enqueue(completion(currentURLResponse, .failure(error)))
-
-        default:
-            return
-        }
-    }
-
-    private func enqueue(_ completion: @escaping @autoclosure () -> Void) {
-        configuration.responseQueue.async {
-            completion()
-        }
-    }
-}
-
-extension Client: DownloadExecutorDelegate {
-    public func downloadExecutor(
-        _ downloadTask: URLSessionDownloadTask,
-        didWriteData bytesWritten: Int64,
-        totalBytesWritten: Int64,
-        totalBytesExpectedToWrite: Int64
-    ) {
-        guard let progressHandler = executingDownloads[downloadTask.identifier]?.progressHandler else { return }
-        enqueue(progressHandler(totalBytesWritten, totalBytesExpectedToWrite))
-    }
-
-    public func downloadExecutor(_ downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        // TODO handle response before calling the completion
-        guard let completionHandler = executingDownloads[downloadTask.identifier]?.completionHandler else { return }
-        enqueue(completionHandler(location, downloadTask.response, downloadTask.error))
-    }
-
-    public func downloadExecutor(_ downloadTask: URLSessionDownloadTask, didCompleteWithError error: Error?) {
-        // TODO handle response before calling the completion
-        guard let completionHandler = executingDownloads[downloadTask.identifier]?.completionHandler else { return }
-        enqueue(completionHandler(nil, downloadTask.response, error))
     }
 }
 
